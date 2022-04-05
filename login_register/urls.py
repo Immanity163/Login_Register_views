787666d7d72bc184f6514432b9419378df8bb2bc
Immanity163 <alexeikurbatov2005@gmail.com>
2022-04-05 12:17:42 +0300

    registration and logout

--- login_register/urls.py
+++ login_register/urls.py
@@ -1,6 +1,11 @@
 from . import views
 from django.urls import path
+from .views import register,authenticated,logout_view,login_view
 
 urlpatterns = [
-    path("login/",views.login,name = 'login')
+    path("login/",views.login_view,name = 'login'),
+    path("register/",register.as_view(),name = 'register'),
+    path("authenticated/",views.authenticated,name = 'index'),
+    path("logout/",views.logout_view,name = 'logout'),
+
 ]
--- login_register/views.py
+++ login_register/views.py
@@ -2,19 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from login_register.forms import Login
-
-def login(request):
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+def login_view(request):
     if request.method == 'POST':
         form = Login(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(username = cd['username'],password = cd[password])
+            user = authenticate(username = cd['username'],password = cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request,user)
-                    return HttpResponse('Authenticated')
+                    return redirect('index')
                 else:
-                    return HttpResponse('some error(actually not)')
+                    return HttpResponse('sorry,you\'re not active ')
             else:
                 return HttpResponse('Invalid Login')
         else: 
@@ -23,3 +27,18 @@ def login(request):
         form = Login()
     return render(request, 'accounts/login.html', {'form': form})
 
+
+class register(FormView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'user_registration.html'
+    def form_valid(self, form):
+        form.save()
+        return super(register, self).form_valid(form)
+
+def authenticated(request):
+    return render(request, "authenticated.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
